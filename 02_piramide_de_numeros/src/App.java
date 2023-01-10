b9b6185d6358864f5a1efe8fbe610c9bb3187caf
Gabriel Cari <g.cari@hotmail.com>
2023-01-09 21:56:09 -0300

    ex 1 e 2

---
+++ 02_piramide_de_numeros/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int iteracoes = 5;
+
+        for (int i = 1; i <= iteracoes; i++){
+            String vez = i;
+            for (int i = 0; i < 5; i++){
+                System.out.println("Hello, World!");
+            }
+        }
+    }
+}
